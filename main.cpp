647a449650525b5687c064bcede16684b3a49e60
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-20 11:51:37 +0000

    Tue, Feb 20, 2024, 5:21 PM +05:30

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
     int a = 5;
+    int *i;
     float b = 2.1;
-    float *c;
-    c = &b;
-    cout << c;
-    cout << c + 1;
+    float *f;
+    i = &a;
+    f = &b;
+    cout << i << endl;
+    cout << f << end;
+    cout << i + f << endl;
     return 0;
 }
