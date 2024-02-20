7314ea5e1c10767aca7baf7e17a2bee5b70319ef
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-20 10:25:28 +0000

    Tue, Feb 20, 2024, 3:55 PM +05:30

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // Define a 3D array with dimensions 2x3x4
-    int threeDArray[2][3][4] = {
-        {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12}
-        },
-        {
-            {13, 14, 15, 16},
-            {17, 18, 19, 20},
-            {21, 22, 23, 24}
-        }
-    };
+int main()
+{
+    // Define a 4D array representing library books
+    int libraryBooks[3][5][10][20]; // Assuming 3 libraries, 5 genres, 10 authors, and 20 publication years
 
-    // Accessing elements of the 3D array
-    cout << "Element at threeDArray[0][1][2]: " << threeDArray[0][1][2] << endl;
-    cout << "Element at threeDArray[1][2][3]: " << threeDArray[1][2][3] << endl;
+    // Populate the array with book counts or other relevant data
+    // For simplicity, let's just initialize it with arbitrary values
+    int counter = 1;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            for (int k = 0; k < 10; ++k)
+            {
+                for (int l = 0; l < 20; ++l)
+                {
+                    libraryBooks[i][j][k][l] = counter++;
+                }
+            }
+        }
+    }
+
+    // Accessing elements of the 4D array
+    cout << "Number of books at library 1, genre 2, author 3, published in year 4: " << libraryBooks[0][1][2][3] << endl;
+    cout << "Number of books at library 3, genre 4, author 7, published in year 10: " << libraryBooks[2][3][6][9] << endl;
 
     return 0;
 }
