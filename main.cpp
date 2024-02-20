f12bddb5082142ba6a9c831937a5eae3d0e81b62
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-20 10:21:44 +0000

    Tue, Feb 20, 2024, 3:51 PM +05:30

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream> // Include iomanip for setting precision
-#include <cmath>
-
+#include <iostream>
 using namespace std;
 
 int main() {
-    int scores[int];
+    // Define a 3D array with dimensions 2x3x4
+    int threeDArray[2][3][4] = {
+        {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        },
+        {
+            {13, 14, 15, 16},
+            {17, 18, 19, 20},
+            {21, 22, 23, 24}
+        }
+    };
 
-    cout << val << endl;
+    // Accessing elements of the 3D array
+    cout << "Element at threeDArray[0][1][2]: " << threeDArray[0][1][2] << endl;
+    cout << "Element at threeDArray[1][2][3]: " << threeDArray[1][2][3] << endl;
+
+    return 0;
 }
