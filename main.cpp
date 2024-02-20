af0d3affc6846f31a1de0a748272e772823b7a97
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-20 11:31:59 +0000

    Tue, Feb 20, 2024, 5:01 PM +05:30

--- main.cpp
+++ main.cpp
@@ -3,28 +3,10 @@ using namespace std;
 
 int main()
 {
-    // Define a 4D array representing library books
-    int libraryBooks[3][5][10][20]; // Assuming 3 libraries, 5 genres, 10 authors, and 20 publication years
-
-    // Populate the array with book counts or other relevant data
-    // For simplicity, let's just initialize it with arbitrary values
-    int counter = 1;
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int j = 0; j < 5; ++j)
-        {
-            for (int k = 0; k < 10; ++k)
-            {
-                for (int l = 0; l < 20; ++l)
-                {
-                    libraryBooks[i][j][k][l] = counter++;
-                }
-            }
-        }
-    }
-
-    // Accessing elements of the 4D array
-    cout << "Number of books at library 1, genre 2, author 3, published in year 4: " << libraryBooks[0][0][0][1] << endl;
-
+    int a = 5;
+    float b = 2.1;
+    float* c;
+    c = &b;
+    cout << c;
     return 0;
 }
