14af96d3602126716e5c80f8ed00a5ba17016fe9
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 13:03:06 +0000

    Wed, Feb 21, 2024, 6:33 PM +05:30

--- sort.cpp
+++ sort.cpp
@@ -5,7 +5,7 @@ void swap(int*, int*);
 void sortArray(int*, int);
 
 int main() {
-    
+
     int n = 5;
     int arr[5] = {1,2,3,4,5};
     cout << "Sorting..." << endl;
@@ -17,5 +17,15 @@ int main() {
     cout << endl;
 
     return 0;
+}
+
+void sortArray(int* arr, int size) {
+    for (int i = 0; i < size-1; i++) {
+        for (int j = i+1; j < size; j++) {
+            if (arr[i] <= arr[j]) {
+                continue;
+            }
+        }
+    }
 }
 
