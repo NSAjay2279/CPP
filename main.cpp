eb955656d7d4ffa89aae83bb9080b0f9f96d078c
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 05:22:53 +0000

    Wed, Feb 21, 2024, 10:52 AM +05:30

--- main.cpp
+++ main.cpp
@@ -3,38 +3,6 @@ using namespace std;
 
 int main()
 {
-    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int b[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 5}};
-    int n = 3;
-
-    bool is_symmetric = true;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (a[i][j] == b[i][j])
-            {
-                continue;
-            }
-            else
-            {
-                is_symmetric = false;
-                break;
-            }
-        }
-        if (!is_symmetric)
-        {
-            break;
-        }
-    }
-    if (is_symmetric)
-    {
-        cout << "symmetric";
-    }
-    else
-    {
-        cout << "not symmetric";
-    }
+    
     return 0;
 }
