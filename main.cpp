a2b0ce6f2b52450ee0b1bbf76018e2bd5c4bd7ef
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 06:36:57 +0000

    Wed, Feb 21, 2024, 12:06 PM +05:30

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ int main()
 {
     string text2 = "Quite simply one of the most";
     string text1 = "beautiful lyrics ever written, ";
+    
+    char a[] = "hello, ";
+    char b[] = "world";
+
+    strcat(a, b);
+
+    cout << a << endl;
+    cout << b << endl;
 
     text1 = text1 + text2;
 
