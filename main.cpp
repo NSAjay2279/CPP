41470cf192da64f1f6be823b4601260ad5ad2bc2
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 08:59:20 +0000

    Wed, Feb 21, 2024, 2:29 PM +05:30

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
     string fullname, firstname, lastname;
-    firstname = 10;
-    lastname = 20;
+    firstname = to_string(10);
+    lastname = to_string(20);
 
     fullname = firstname + lastname;
 
+    cout << firstname << endl;
+    cout << lastname << endl;
     cout << fullname << endl;
     cout << fullname.length() << endl;
     cout << fullname.size() << endl;
