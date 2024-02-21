a36078aee17bb24b468fec7ceb72caf8ca3ea332
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 13:43:05 +0000

    Wed, Feb 21, 2024, 7:13 PM +05:30

---
+++ complex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct complex_number {
+    int real;
+    int imag;
+};
+int main() {
+    complex_number num;
+    num.real = 20;
+    num.imag = 10;
+}
+
