6a9f17b92ef554e3830501c9eea5b1530d5a4be7
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 04:54:53 +0000

    Wed, Feb 21, 2024, 10:24 AM +05:30

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 int main()
 {
-    a[5] = {1,2,3,4,5};
-    b[5] = {1,2,3,4,6};
+    int a[5] = {1,2,3,4,5};
+    int b[5] = {1,2,3,4,5};
+    int n = 5;
 
     bool is_equivalent = true;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (a[i] == b[j]) {
                 continue;
+            } else {
+                is_equivalent = false;
+                break;
             }
         }
+        if (!is_equivalent) {
+            break;
+        }
+    }
+    if (is_equivalent) {
+        cout << "equivalent";
+    } else {
+        cout << "not equivalent";
     }
     return 0;
 }
