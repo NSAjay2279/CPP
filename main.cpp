8ede8872c9becce5cd4b6d34ec60b4f567a0c94f
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 05:34:52 +0000

    Wed, Feb 21, 2024, 11:04 AM +05:30

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    
+    char lyrics2[] = "tu raat diwani, mai zardsitara";
+    char lyrics1[] = "oh tere sang yaara khush ranga bhara, ";
+
+    strcat(lyrics1, lyrics2);
+
+    cout << lyrics1 << endl;
+    cout << lyrics2 << endl;
     return 0;
 }
