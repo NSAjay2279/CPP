7cc833ba9283da0e84be517cf8f5e7fd5fdfeae5
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 12:32:59 +0000

    Wed, Feb 21, 2024, 6:02 PM +05:30

---
+++ swap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void swam_m(int, int);
+
+int main() {
+    int a, b;
+    cout << "Enter values of a & b" << endl;
+    cin >> a >> b;
+    swamp_m(a, b);
+    cout << "Outside Fn, A: " << a << endl << "B: " << b << endl;
+    cout << "X IS: " << x << endl;
+
+    return 0
+}
+
+void swap_m(int *a, int *b) {
+    int temp;
+    temp = *b;
+    *b = *a;
+    *a = temp;
+    cout << "Inside Fn, a: " << a << ". And b is: " << b << endl;
+}
