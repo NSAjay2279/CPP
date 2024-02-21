53ee5a53931d158fc2a0899e658bb2bd27303083
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 15:00:51 +0000

    Wed, Feb 21, 2024, 8:30 PM +05:30

--- complex.cpp
+++ complex.cpp
@@ -5,14 +5,24 @@ struct complex_number {
     int real;
     int imag;
 };
+
+complex_number adding_complex(complex_number, complex_number);
+
 int main() {
     complex_number num1, num2, num3;
     num1.real = 20;
     num1.imag = 10;
     num2.real = 30;
     num2.imag = 40;
-    num3.real = num1.real + num2.real;
-    num3.imag = num1.imag + num2.imag;
+    num3 = adding_complex(num1, num2);
+    cout << num3.real << " + i" << num3.imag;
+
+    return 0;
 }
 
-
+complex_number adding_complex(complex_number a, complex_number b) {
+    complex_number outpt;
+    outpt.real = a.real + b.real;
+    outpt.imag = a.imag + b.imag;
+    return outpt;
+}
