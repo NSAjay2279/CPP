58af3911218e6e810e18c11b573877fea4f79f1f
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-21 13:51:58 +0000

    Wed, Feb 21, 2024, 7:21 PM +05:30

--- complex.cpp
+++ complex.cpp
@@ -6,8 +6,13 @@ struct complex_number {
     int imag;
 };
 int main() {
-    complex_number num;
-    num.real = 20;
-    num.imag = 10;
+    complex_number num1, num2, num3;
+    num1.real = 20;
+    num1.imag = 10;
+    num2.real = 30;
+    num2.imag = 40;
+    num3.real = num1.real + num2.real;
+    num3.imag = num1.imag + num2.imag;
 }
+
 
