06d6f7b604be4f79fccc236692ed5cb3ed50a672
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-22 13:07:37 +0000

    Thu, Feb 22, 2024, 6:37 PM +05:30

--- pointers.cpp
+++ pointers.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main() {
     // This code allocates two pointers, which can point to integers
-    int *x;
+    int *x = 42;
     int *y;
     // initially pointers don't point to anything
     // The things pointers point to are called pointees
+    // 
 }
