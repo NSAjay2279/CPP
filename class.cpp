8a98878d44e94d2821a7a40c4a4f35f57f26fd53
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-22 06:56:19 +0000

    Thu, Feb 22, 2024, 12:26 PM +05:30

---
+++ class.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Testing {
+  private:
+    int a;
+    float b;
+
+  public:
+    int c;
+    float d;
+    void set_default(void) {
+        a = 0;
+        b = 0;
+    }
+    void print_values(void) {
+        cout << "a: " << a << endl;
+        cout << "b: " << b << endl;
+        cout << "c: " << c << endl;
+        cout << "d: " << d << endl;
+    }
+};
+
+int main() {
+    Testing x;
+    x.c = 5;
+    x.d = 8;
+    x.set_default();
+    x.print_values();
+    return 0;
+}
