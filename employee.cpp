4410fc7611efd15df4f86d32e8e68100aa3991fe
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-02-25 06:52:26 +0000

    Sun, Feb 25, 2024, 12:22 PM +05:30

--- employee.cpp
+++ employee.cpp
@@ -6,33 +6,37 @@ class Employee {
     string name;
 
   public:
-    friend ostream &operator<<(ostream &out, Employee &c);
+    friend ostream &operator<<(ostream &out, const Employee &c);
     friend istream &operator>>(istream &in, Employee &c);
-    void operator=(Employee &);
+    void operator=(const Employee &b);
 };
 
-int main() {
+int main()
+{
     Employee a, b;
     cin >> a;
-    cout >> a;
+    cout << a;
     b = a;
     cout << b;
 
     return 0;
 }
-ostream &operator<<(ostream &out, Employee &c) {
+
+ostream &operator<<(ostream &out, const Employee &c)
+{
     out << "--- EMPLOYEE RECORD ---" << endl;
     out << "NAME: " << c.name << endl;
     return out;
 }
 
-istream &operator>>(istream &in, Employee &c) {
+istream &operator>>(istream &in, Employee &c)
+{
     cout << "Enter name of employee" << endl;
     in >> c.name;
     return in;
 }
 
-void Employee::operator = (Employee& b) {
+void Employee::operator=(const Employee &b)
+{
     name = b.name;
-    return;
 }
