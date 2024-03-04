11c377dc5a5c55eef6c2a275f60bd27a87631959
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-04 15:23:58 +0000

    Mon, Mar 4, 2024, 8:53 PM +05:30

--- Python-main/0. Function, Variables/calculator.cpp
+++ Python-main/0. Function, Variables/calculator.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+using namespace std;
+
 int square(int n) {
 	return n*n;
 	}
 
 int main() {
-	int x = 10;
-	cout << square(x) << endl;
+	int x;
+	cout << "what's x? ";
+	cin >> x;
+	cout << "x square is " << square(x) << endl;
 	return 0;
 }
