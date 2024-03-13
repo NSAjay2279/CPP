37482a04d7d0d276f2a889165dba77a7c49379f2
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-13 17:09:37 +0000

    Wed, Mar 13, 2024, 10:39 PM +05:30

---
+++ luckyfour.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int counter = 0;
+    cout << "Enter a single integer T: " << endl;
+    cin >> T;
+    cout << "Enter " << n << "lines of integers: " << endl;
+    for (int i = 0; i < T; T++){
+        cin >> T;
+    }
+
+    for (int i = 0; i < num1.length(); i++)
+    {
+        if (num1[i] == '4')
+        {
+            counter++;
+        }
+    }
+    cout << counter << endl;
+
+    counter = 0;
+    for (int i = 0; i< num2.length(); i++)
+    {
+        if (num2[i] == '4')
+        {
+            counter++;
+        }
+    }
+    cout << counter << endl;
+
+    return 0;
+}
