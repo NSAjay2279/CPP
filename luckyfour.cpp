3af1680d50785f1c139eb559129442efd339c730
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-13 17:37:53 +0000

    Wed, Mar 13, 2024, 11:07 PM +05:30

--- luckyfour.cpp
+++ luckyfour.cpp
@@ -4,34 +4,18 @@
 using namespace std;
 
 int main(){
+  int n=3;
   int counter = 0;
-  string num1 = "4444";
-  string num2 = "2834";
-  string num3 = "876";
+  string num[3] = {"4444","2834","876"};        // Dynamic programming required
 
-  for(int i=0; i<num1.length(); i++){
-    if(num1[i] == '4'){
-      counter++;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<num[i].length(); j++){
+      if(num[i][j] == '4'){
+        counter++;
+      }
     }
+    cout << counter << endl;
+    counter = 0;
   }
-  cout << counter << endl;
-
-  counter = 0;
-  for(int i=0; i<num2.length(); i++){
-    if(num2[i] == '4'){
-      counter++;
-    }
-  }
-  cout << counter << endl;
-
-  counter = 0;
-  for(int i=0; i<num3.length(); i++){
-    if(num3[i] == '4'){
-      counter++;
-    }
-  }
-  cout << counter << endl;
-
-
   return 0;
 }
