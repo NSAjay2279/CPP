c4560c4c222e3f4a3a2eb377f1ef98a669d3938d
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-14 19:13:18 +0530

    luckyfour.cpp

--- luckyfour.cpp
+++ luckyfour.cpp
@@ -4,11 +4,20 @@
 using namespace std;
 
 int main(){
-  int n=3;
+  int t;
   int counter = 0;
-  string num[3] = {"4444","2834","876"};        // Dynamic programming required
-
-  for(int i=0; i<n; i++){
+  // Dynamic programming required
+  cout << "Enter a number t: ";
+  cin >> t;
+  
+  string num[t] = {};
+  
+  cout << "Enter list of " << t << " lines of numbers of varying range as you wish: " << endl;
+  for(int i=0; i<t; i++){
+      cin >> num[i];
+  }
+  
+  for(int i=0; i<t; i++){
     for(int j=0; j<num[i].length(); j++){
       if(num[i][j] == '4'){
         counter++;
